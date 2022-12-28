ed888964fb79e76fc9b35a70fd00611c32050d64
Yudhitia Rizky <yudhitiarizky2002@gmail.com>
2022-12-28 20:21:51 +0700

    ok

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
 const router = require('./api/index')
 
 const PORT = process.env.PORT || 5000;
+const public = __dirname + "/public/";
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/public/uploads', express.static(path.join(public, "uploads")));
 
 app.use('/', router);
 
